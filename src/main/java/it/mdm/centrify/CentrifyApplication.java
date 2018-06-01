d33e63bbd8823c7034cb3806f535715d5cf3a847
Massimiliano Bruni <maxbrux@live.it>
2018-06-01 12:39:19 +0200

    Switched to Spring

---
+++ src/main/java/it/mdm/centrify/CentrifyApplication.java
@@ -0,0 +1,12 @@
+package it.mdm.centrify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentrifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentrifyApplication.class, args);
+	}
+}
