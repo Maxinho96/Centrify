d51fb128f132bcd3af0e87cd301a6555928ae9a6
Massimiliano Bruni <maxbrux@live.it>
2018-06-19 16:17:02 +0200

    Sistemato database

--- src/main/java/it/mdm/centrify/controller/DirettoreController.java
+++ src/main/java/it/mdm/centrify/controller/DirettoreController.java
@@ -4,7 +4,6 @@ import java.security.Principal;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -30,7 +29,7 @@ public class DirettoreController {
 	@Autowired
 	private CentroService centroService;
 	
-	@Component
+	@Autowired
 	private CentroValidator centroValidator;
 
 	@ModelAttribute("direttore")
